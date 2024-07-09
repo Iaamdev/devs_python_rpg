415453c4700e8a7c0abe6fe25edc328b741758f1
Iaamdev <139625151+Iaamdev@users.noreply.github.com>
2024-07-10 00:16:51 +0900

    Updated main_game.py

--- main_game.py
+++ main_game.py
@@ -1,27 +1,46 @@
 from Fighter import Fighter
 
+# separator for text
+BORDER = "-" * 50
+
 game_start = True
 player_class = ""
 
 print("Welcome to Dev's Python RPG\n")
-choose_class = input("Choose Your Starting Class: \n\nFIGHTER//MAGE//THIEF:\n").upper()
+choose_class = input(
+    "Choose Your Starting Class: \n\nFIGHTER ⚔️// MAGE 🪄//THIEF 🗡️:\n"
+).upper()
 
 fighter_player = Fighter()
 # mage_player =
 # thief_player =
+print(BORDER)
 if choose_class == "FIGHTER":
+    print("Fighter Chosen!!\n")
     player_class = fighter_player
+    fighter_player.status_bars()
+print(BORDER)
 
 # TODO: make a different "if" loop for each class
 # NOTE: Currently working on fighter class hence fighter only path below
 while game_start:
-    # NOTE: was debugging my fighter_skill_up method
+    # NOTE: currently debugging my fighter_skill_up method
     if player_class == fighter_player:
         if fighter_player.xp == 20:
+            print(BORDER)
             fighter_player.lvl_up()
+            fighter_player.status_bars()
+            print(BORDER)
             fighter_player.fighter_skill_up()
-            print(fighter_player.current_skills)
-            game_start = False
+            print(BORDER)
+            # NOTE: a current loop for testing the lvl up sys. to
+            # stop code
+            continue_game = input("Continue? (y/n):\n").upper()
+            if continue_game == "Y":
+                pass
+            elif continue_game == "N":
+                game_start = False
+            # game_start = False
         fighter_player.xp += 20
 
 # TODO: continue working on fighter class
