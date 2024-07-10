f21bbf38023cb9ea3c6a446151877e889ec964ca
Iaamdev <139625151+Iaamdev@users.noreply.github.com>
2024-07-10 23:21:16 +0900

    Update main_game.py
    resolved fighter_skill_up method//Work on an enemy for combat function creation

--- main_game.py
+++ main_game.py
@@ -15,32 +15,42 @@ fighter_player = Fighter()
 # mage_player =
 # thief_player =
 print(BORDER)
+
 if choose_class == "FIGHTER":
     print("Fighter Chosen!!\n")
     player_class = fighter_player
     fighter_player.status_bars()
 print(BORDER)
 
+
 # TODO: make a different "if" loop for each class
 # NOTE: Currently working on fighter class hence fighter only path below
 while game_start:
-    # NOTE: currently debugging my fighter_skill_up method
+
     if player_class == fighter_player:
+
         if fighter_player.xp == 20:
-            print(BORDER)
             fighter_player.lvl_up()
             fighter_player.status_bars()
             print(BORDER)
-            fighter_player.fighter_skill_up()
-            print(BORDER)
-            # NOTE: a current loop for testing the lvl up sys. to
-            # stop code
-            continue_game = input("Continue? (y/n):\n").upper()
-            if continue_game == "Y":
-                pass
-            elif continue_game == "N":
-                game_start = False
-            # game_start = False
-        fighter_player.xp += 20
 
-# TODO: continue working on fighter class
+            if fighter_player.level % 3 == 0:
+                fighter_player.fighter_skill_up()
+
+        fighter_player.xp += 5
+
+        print("The Traveling Continues...")
+        # NOTE: a current loop for testing the lvl up sys. to
+        # stop code
+        continue_game = input("Continue? (y/n):\n").upper()
+        print(BORDER)
+        if continue_game == "Y":
+            pass
+        elif continue_game == "N":
+            game_start = False
+
+
+# TODO: Continue working on fighter class
+# TODO: Make an enemy to test out combat functions
+# TODO: Make descriptions of class skills in anohter file
+# for the player to then pull at the skill selection chooser
