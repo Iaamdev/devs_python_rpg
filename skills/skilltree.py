4f3f8889b619ead2dbdf7b6eacb229a87a91fde8
Iaamdev <139625151+Iaamdev@users.noreply.github.com>
2024-07-17 19:06:36 +0900

    quick push

--- skills/skilltree.py
+++ skills/skilltree.py
@@ -1,14 +1,41 @@
-import pandas
+import sys
 
-import Fighter as f
+import pandas as pd
+
+# FIX: figure out how to import from different file path
+from Devs_PythonRPG.Fighter import Fighter
 
 BORDER = "-" * 50
-data = pandas.read_csv("new_skills_list_test.csv")
-# skill_list = data.Skill.to_list()
-# skill_descript = data.Description.to_dict()
-skill_list = data["Skill"].to_list()
+fighter_data = pd.read_csv("fighter_sd.csv")
+mage_data = pd.read_csv("mage_sd.csv")
+thief_data = pd.read_csv("thief_sd.csv")
+f = Fighter
 
-print(skill_list)
+print(f.warrior_skills)
+
+
+def skill_check():
+    skill_check_complete = False
+
+    # if f.level < 15:
+    #     print(f"{fighter_skills}\n")
+    # elif f.level > 15 and is_paladin:
+    #     print(f"{paladin_skills}\n")
+    # elif f.level > 15 and is_warrior:
+    #     print(f"{warrior_skills}\n")
+
+    while skill_check_complete:
+        # ask for user input on what skill they would like to view
+        skill_choice = input("What skill would you like to view?:\n").upper()
+        # if their choice is == any skill within their class show
+        # the class description
+        # if skill_choice == fighter_data.Skill:
+        print(fighter_data.Skill)
+    # ask the user if they would like to look at another skill
+    # if not cancel the loop
+
+
+# skill_check()
 
 # TODO: USE NEW LIST BELOW TO PULL THE SKILL DESCRIPTION
 # BASED ON skill_description_choice
